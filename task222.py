4ff77ee04c2a416c7a4aa6826a053368f0db3fc8
SerhiiKorniichuk1 <99148205+SerhiiKorniichuk1@users.noreply.github.com>
2022-11-22 17:09:36 +0200

    Update task222.py

--- task222.py
+++ task222.py
@@ -1,27 +1,30 @@
 from mpi4py import MPI
 from random import uniform
 
+from mpi4py import MPI
+from random import uniform
+
 comm = MPI.COMM_WORLD
 #comm is the communicator object. Its methods will be used
 #to carry out between processes.
 
 rank = comm.Get_rank()
 S = comm.Get_size() #number of processes
-N = 20#number of points
+N = 10 #number of points
 
 if rank == 0:
     points = tuple([(uniform(0,1), uniform(0,1)) for i in range(N)])
     
     #send
     for i in range(1, S): #from 1 to number of processes
-        comm.send(points[0 : i * N // (S-1)], dest=1, tag=1)
+        comm.send(points[0 : i * N // (S-1)], dest=i, tag=i)
     
     #receive
     for i in range(1, S):
         PI, time = comm.recv(source=i, tag=i)
-        print(i)
-        print(PI)
-        print(time)
+        print("iteration:", i)
+        print("value of the number:", PI)
+        print("time:", time)
 else:
     points = comm.recv(source=0, tag=rank)
     start = MPI.Wtime() #return an elapsed time on the calling processor
